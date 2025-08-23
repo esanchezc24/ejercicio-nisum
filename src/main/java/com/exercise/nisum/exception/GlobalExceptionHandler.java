7cfd1101d08000744232980461903d739b069832
Erick <esanchezc24gmail.com>
2025-08-22 22:34:15 -0500

    Se agrego readme

--- src/main/java/com/exercise/nisum/exception/GlobalExceptionHandler.java
+++ src/main/java/com/exercise/nisum/exception/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.exercise.nisum.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.time.LocalDateTime;
 
--- src/main/java/com/exercise/nisum/service/user/UserServiceImpl.java
+++ src/main/java/com/exercise/nisum/service/user/UserServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
